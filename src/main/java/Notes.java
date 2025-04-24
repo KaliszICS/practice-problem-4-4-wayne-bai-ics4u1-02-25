daf2d72df74793513b6dc18db5b6b283f0a4a154
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-04-24 13:39:42 +0000

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,6 +1,8 @@
 
 class Notes {
 	public static void main(String[] args) {
-		
+		System.out.println(MyUtilityClass.MAXIMUM);
+		System.out.println(MyUtilityClass.returnZero());
+		System.out.println(MyUtilityClass.MAXIMUM);
 	}
 }
